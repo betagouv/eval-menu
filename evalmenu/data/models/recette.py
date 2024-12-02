cb3cc4ea2ca9768eb8bfbfd38d70c5b89e963dc2
qloridant <quentin.loridant@protonmail.com>
2024-12-02 17:30:37 +0100

    Ajout champ cs, nutriscore et type plat

--- evalmenu/data/models/recette.py
+++ evalmenu/data/models/recette.py
@@ -2,9 +2,25 @@ from django.db import models
 from data.models.ingredient import Ingredient
 
 class Recette(models.Model):
+
+    class NutriScore(models.TextChoices):
+        A = "A",
+        B = "B",
+        C = "C",
+        D = "D"
+        E = "E"
+
+    class TypePlat(models.TextChoices):
+        main = "main"
+        starter = "starter"
+        desert = "desert"
+
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
     ingredients = models.ManyToManyField(Ingredient, through="RecetteIngredient")
+    nutriscore = models.CharField(choices=NutriScore, max_length=1, null=True)
+    cs = models.IntegerField(help_text="Score environemental", default=0, null=True)
+    type_plat = models.CharField(choices=TypePlat, max_length=30, null=True)
 
     def __str__(self):
         return self.name
