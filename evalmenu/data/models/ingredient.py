b4c0c11c512cc9dc939f72df5785437c919ed48a
qloridant <quentin.loridant@protonmail.com>
2024-12-02 17:01:43 +0100

    Surcharge méthode str() des modèles et correction champ typo ingredient

--- evalmenu/data/models/ingredient.py
+++ evalmenu/data/models/ingredient.py
@@ -6,3 +6,6 @@ class Ingredient(models.Model):
     id_ecobalyse = models.CharField(max_length=30)
     name = models.CharField(max_length=30, unique=True)
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- evalmenu/data/models/recette.py
+++ evalmenu/data/models/recette.py
@@ -6,8 +6,14 @@ class Recette(models.Model):
     name = models.CharField(max_length=30)
     ingredients = models.ManyToManyField(Ingredient, through="RecetteIngredient")
 
+    def __str__(self):
+        return self.name
+
 
 class RecetteIngredient(models.Model):
     recette = models.ForeignKey(Recette, on_delete=models.CASCADE)
-    igredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     poids = models.DecimalField(max_digits=10, decimal_places=2, help_text="Poids (en gramme)")
+
+    def __str__(self):
+        return f"{self.recette} - {self.ingredient}"
