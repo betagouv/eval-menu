ec7c65e41cbc81b98c2bc3b4b8aa903d2de90d42
qloridant <quentin.loridant@protonmail.com>
2024-12-02 17:08:27 +0100

    Allonger le ecobalyse id

--- evalmenu/data/models/ingredient.py
+++ evalmenu/data/models/ingredient.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Ingredient(models.Model):
     id = models.IntegerField(primary_key=True)
-    id_ecobalyse = models.CharField(max_length=30)
+    id_ecobalyse = models.CharField(max_length=40)
     name = models.CharField(max_length=30, unique=True)
     description = models.TextField(blank=True, null=True)
 
