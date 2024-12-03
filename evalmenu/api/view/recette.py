294e527151ace3c20661474f3094c7b3e02a170c
qloridant <quentin.loridant@protonmail.com>
2024-12-03 10:56:46 +0100

    Ajout méthode create pour les recette

--- evalmenu/api/view/recette.py
+++ evalmenu/api/view/recette.py
@@ -1,15 +1,31 @@
 from django.contrib.auth.models import Group, User
-
+from rest_framework import viewsets
 from rest_framework.generics import (
-    ListAPIView,
+    ListAPIView, 
 )
+from rest_framework import status
+from rest_framework.response import Response
 from data.models.recette import Recette
 from api.serializers.recette import RecetteSerializer
+from rest_framework.permissions import AllowAny
 
-
-class RecetteView(ListAPIView):
+class RecetteViewSet(viewsets.ViewSet):
     """
     API endpoint that allows recette to be viewed.
     """
     queryset = Recette.objects.all()
     serializer_class = RecetteSerializer
+    permission_classes = [AllowAny]  # Allow all requests  
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()  # Fetch all objects
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
